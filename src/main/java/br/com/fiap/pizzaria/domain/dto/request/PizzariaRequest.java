eb0fec0822803e75f899982cc24d4417abf311cc
MunirAyoub <rm550893@fiap.com.br>
2024-04-30 17:56:21 -0300

    service feito

--- src/main/java/br/com/fiap/pizzaria/domain/dto/request/PizzariaRequest.java
+++ src/main/java/br/com/fiap/pizzaria/domain/dto/request/PizzariaRequest.java
@@ -1,6 +1,9 @@
 package br.com.fiap.pizzaria.domain.dto.request;
 
+import br.com.fiap.pizzaria.domain.entity.Produto;
 import jakarta.validation.constraints.NotNull;
+
+import java.util.Set;
 
 public record PizzariaRequest(
         @NotNull(message = "O nome não pode ser null")
